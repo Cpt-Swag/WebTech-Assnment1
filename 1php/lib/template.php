6b50e4a81d7b7aa072426fffd29dfd0461e8c885
King Bas <Mr.Bascombe@live.com>
2016-03-02 12:56:49 -0400

    Added headders to the tables

--- 1php/lib/template.php
+++ 1php/lib/template.php
@@ -10,15 +10,19 @@ function makeList($listStuff)
 
 // A user defined function to take a multidimensional array and put its data into an HTML table
 function makeTabel($tableStuff)
-{
-        foreach ($tableStuff as $word => $value) {
+{       
         echo '<table class="table table-bordered" style="width:50%">';
+        echo "<tr>";
+            echo "<th>" . "WORD" . "</th>";
+            echo "<th>" . "SCORE" . "</th>";
+        echo "</tr>";
+        foreach ($tableStuff as $word => $value) {
             echo "<tr>";
                 echo "<td>" . $word . "</td>";
                 echo "<td>" .  $value . "</td> ";
             echo "</tr>";
-        echo '</table>';
         }
+         echo '</table>';
 }
 	
 ?>
